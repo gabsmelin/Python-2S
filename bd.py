a5ae7beb5c47a43ef95b53bf8ed634f9270b418f
gabsmelin <rm550202@fiap.com.br>
2023-09-29 16:57:57 -0300

    tentando arrumar conexão

--- bd.py
+++ bd.py
@@ -1,25 +1,14 @@
-import sqlalchemy as db 
+import sqlalchemy as db
 
-engine = db.create_engine("sqlite:///1tdss.db")
-#engine = db.create_engine("sqlite:///Users/Gabriel/Documents/sqlite-tools-win32-x86-3430100/1tdss.db")
-#engine = db.create_engine("sqlite:///C:\\SQlite3\\1tdss.db")
+#engine = db.create_engine("sqlite:///1tdss.db")
+#engine = db.create_engine("sqlite:////absolute/path/to/1tdss.db") 
+engine = db.create_engine("sqlite:///X:\\Programação\\FIAP\\2023\\Python\\SQLite") 
 
-connect = engine.connect()
 metadata = db.MetaData()
-aluno = db.Table("alunos", metadata, autoload_with=engine)
+aluno = db.Table("alunos", metadata, autoload=True, autoload_with=engine)
 print(aluno.columns.keys())
+query = db.insert(aluno).values(id=1, name="Alberico", turma="1tdss", obs='nada')
+ResultProxy = db.connect.execute(query)
 
-#emp = db.Table("alunos", metadata, 
-#            db.Column("id", db.Integer()),
-#            db.Column("name", db.Text()),
-#            db.Column("turma", db.Text()),
-#            db.Column("obs", db.Text()))
-#
-#jog = db.Table()
-#metadata.create_all(engine)
-
-query = db.insert(aluno).values(id=1, name="Gabriel", turma="1TDSS", obs='Lindo')
-ResultProxy = connect.execute(query)
-
-resultado = connect.execute(db.select([aluno])).fetchall()
+resultado = db.connect.execute(db.select([aluno])).fetchall()
 print(resultado)
