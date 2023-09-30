c1e19fe0f7c1254f7fa4c29aeacb85f8d1370c25
gabsmelin <rm550202@fiap.com.br>
2023-09-30 20:43:04 -0300

    desen volvendo uma api local

--- api_flask.py
+++ api_flask.py
@@ -7,7 +7,7 @@ api = Api(app)
 
 lista = ["lucas"]
 
-class listar(Resource):
+class Listar(Resource):
     def get(self):
         return str(lista)
 
@@ -15,4 +15,20 @@ class Main(Resource):
     def post(self, nome):
         lista.append(str(nome).title())
         return f'Usúario { nome } foi adicionado com suceeso!'
-         
+    def delete(self, nome):
+        if nome in lista:
+            for index in range(0, len(lista)):
+                if lista[index] == nome:
+                    del lista[index]
+                    return "Elemento removido!"
+                else:
+                    return "Elemento não existe!"
+                
+class Version(Resource):
+    def get(self):
+        return "0.0.1"
+    
+
+api.add_resource(Listar, "/")
+api.add_resource(Main, "/<nome>")
+api.add_resource(Version, "/version")
--- bd.py
+++ bd.py
@@ -1,14 +1,33 @@
 import sqlalchemy as db
 
-#engine = db.create_engine("sqlite:///1tdss.db")
-#engine = db.create_engine("sqlite:////absolute/path/to/1tdss.db") 
-engine = db.create_engine("sqlite:///X:\\Programação\\FIAP\\2023\\Python\\SQLite") 
-
+engine = db.create_engine("sqlite:///1tdss.db")
+connect = engine.connect()
 metadata = db.MetaData()
-aluno = db.Table("alunos", metadata, autoload=True, autoload_with=engine)
-print(aluno.columns.keys())
-query = db.insert(aluno).values(id=1, name="Alberico", turma="1tdss", obs='nada')
-ResultProxy = db.connect.execute(query)
 
-resultado = db.connect.execute(db.select([aluno])).fetchall()
+# Defina explicitamente a estrutura da tabela "alunos"
+aluno = db.Table("alunos", metadata,
+    db.Column("id", db.Integer),
+    db.Column("turma", db.String),
+    db.Column("name", db.String),
+    db.Column("obs", db.String)
+)
+
+print(aluno.columns.keys())
+
+# Crie um dicionário com os valores que deseja inserir
+values = {
+    "id": 1,
+    "turma": "1tdss",
+    "name": "melo",
+    "obs": 'lindo'
+}
+
+# Crie a consulta de inserção com os valores
+query = db.insert(aluno).values(**values)
+ResultProxy = connect.execute(query)
+
+# Corrija a consulta SELECT para selecionar todas as colunas da tabela "alunos"
+select_query = db.select(aluno)
+resultado = connect.execute(select_query).fetchall()
+
 print(resultado)
