bec2915ac2920385ce4155d7ac0c5bea4d23a52c
gabsmelin <rm550202@fiap.com.br>
2023-10-03 22:00:04 -0300

    Conexão com o banco de dados feita

--- bd.py
+++ bd.py
@@ -1,33 +1,40 @@
 import sqlalchemy as db
 
 engine = db.create_engine("sqlite:///1tdss.db")
+
+
 connect = engine.connect()
 metadata = db.MetaData()
+metadata.bind = engine
+metadata.reflect(bind=engine)
 
-# Defina explicitamente a estrutura da tabela "alunos"
-aluno = db.Table("alunos", metadata,
-    db.Column("id", db.Integer),
-    db.Column("turma", db.String),
-    db.Column("name", db.String),
-    db.Column("obs", db.String)
-)
+alunos = db.Table("alunos", metadata,
+        db.Column('id', db.Integer, primary_key=True),
+        db.Column('nome', db.String), db.Column('turma', db.String), db.Column('obs', db.String))
 
-print(aluno.columns.keys())
+print(alunos.columns.keys())
 
-# Crie um dicionário com os valores que deseja inserir
-values = {
-    "id": 1,
-    "turma": "1tdss",
-    "name": "melo",
-    "obs": 'lindo'
-}
 
-# Crie a consulta de inserção com os valores
-query = db.insert(aluno).values(**values)
-ResultProxy = connect.execute(query)
-
-# Corrija a consulta SELECT para selecionar todas as colunas da tabela "alunos"
-select_query = db.select(aluno)
-resultado = connect.execute(select_query).fetchall()
-
-print(resultado)
+#aluno = db.Table("alunos", metadata,
+#    db.Column("id", db.Integer),
+#    db.Column("turma", db.String),
+#    db.Column("name", db.String),
+#    db.Column("obs", db.String)
+#)
+#
+#print(aluno.columns.keys())
+#
+#values = {
+#    "id": 1,
+#    "turma": "1tdss",
+#    "name": "melo",
+#    "obs": 'lindo'
+#}
+#
+#query = db.insert(aluno).values(**values)
+#ResultProxy = connect.execute(query)
+#
+#select_query = db.select(aluno)
+#resultado = connect.execute(select_query).fetchall()
+#
+#print(resultado)
--- flask_com_db.py
+++ flask_com_db.py
@@ -2,16 +2,26 @@ from flask import Flask
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 
+#Criamos o APP
 app = Flask(__name__)
+
+#Jogamos uma API dentro do APP
 api = Api(app)
+
+#Passamos configurações direto para o app, para não afetar o código
+##Passamos a API e passamos o nosso banco de dados como 'valor'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///1tdss.db"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "oracle+driver://username:password@host:port/dbname"  <--- Caso usamos o banco de dados da oracle
+##Passamos um TRACK para 'salvar' alterações
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Usamos Model para instanciar um método, no caso ele está no App e por isso, a partir de agora db tem a classe model
 db = SQLAlchemy(app)
 
-db = SQLAlchemy(app)
 
-
+#Usamos Model para instanciar um método
 class Aluno(db.Model):
+    #Passamos a tabela(e o nome dela) a ser seguida e em seguida seus parâmetros/colunas da tabela
     _tablename_= 'aluno'
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(50), nullable=False)
@@ -19,34 +29,31 @@ class Aluno(db.Model):
     curso = db.Column(db.String)
     departamento = db.Column(db.String)
 
+    #Redefine o metódo reconstrutor da tabela
+    def __init__(self, nome, turma, obs):
+        self.nome = nome
+        self.turma = turma
+        self.obs = obs
+
+#adiciona os dados a tabela
+app.app_context().push()
+#Cria uma nova tabela
+db.create_all()
 
 
-
-
-class Listar(Resource):
-    def get(self):
-        return str(lista)
     
 class Main(Resource):
-    def post(self, nome):
-        lista.append(str(nome).title())
-        return f'Usuario {nome} foi adicionado com sucesso!'
-    def delete(self, nome):
-        if nome in lista:
-            for index in range(0, len(lista)):
-                if lista[index] == nome:
-                    del lista[index]
-                    return "Elemento removido"
-        else:
-            return "Elemento não existe"
+    def get():
+        pass
+    def post():
+        ...
+    def put():
+        ...
+    def delete():
+        ...
+        
 
-class Version(Resource):
-    def get(self):
-        return "0.0.1"
-
-api.add_resource(Listar, "/")
 api.add_resource(Main, "/<nome>")
-api.add_resource(Version, "/version")
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(port=8080)
