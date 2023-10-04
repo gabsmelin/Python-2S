4720310c9b77a7f9a02bd30a9ba1fec084ac87a3
gabsmelin <rm550202@fiap.com.br>
2023-10-03 22:12:33 -0300

    Método get feito

--- flask_com_db.py
+++ flask_com_db.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 
@@ -23,11 +23,10 @@ db = SQLAlchemy(app)
 class Aluno(db.Model):
     #Passamos a tabela(e o nome dela) a ser seguida e em seguida seus parâmetros/colunas da tabela
     _tablename_= 'aluno'
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(50), nullable=False)
-    cpf = db.Column(db.integer, unique= True)
-    curso = db.Column(db.String)
-    departamento = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
+    nome = db.Column(db.Text, nullable = False)
+    turma = db.Column(db.Text, nullable = False)
+    obs = db.Column(db.Text, nullable = False)
 
     #Redefine o metódo reconstrutor da tabela
     def __init__(self, nome, turma, obs):
@@ -44,14 +43,17 @@ db.create_all()
     
 class Main(Resource):
     def get():
-        pass
-    def post():
-        ...
-    def put():
-        ...
-    def delete():
-        ...
-        
+        #Pegamos todos os dados classe aluno
+        alunos = Aluno.query.all()
+        lista = []
+        #Aqui alunos se transforma em aluno e fica organizado linha por linha, listando um aluno por vez.
+        for aluno in alunos:
+            print(aluno)
+            #aqui damos append no dicionario(adicionamos coluna por coluna. ex: {("id": aluno.id, "nome": "Alberico", "turma"="1tdss", "obs":"Prof")}
+            #fazemos isso para ficar um dicionario por linha, dentro de um json
+            lista.append({'id': aluno.id, 'nome': aluno.nome, 'turma':aluno.turma, 'obs':aluno.obs})
+        return jsonify(lista)
+   
 
 api.add_resource(Main, "/<nome>")
 
